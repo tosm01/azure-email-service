3d81eb53431e28a6eab66f5ecd381cd34bd4a242
tosm01 <taha2013.to@gmail.com>
2022-11-16 00:42:49 +0800

    Added flexibility to use any registered sender

--- emailservice/api/views.py
+++ emailservice/api/views.py
@@ -5,14 +5,18 @@ from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from env import CONNECTION_STRING
-
+from env import CONNECTION_STRINGS
 
 class EmailAPI(APIView):
     def post(self, request, *args, **kwargs):
+        sender = request.data.get('sender')
+        
+        if sender not in CONNECTION_STRINGS:
+            return HttpResponse(content="Sender is not registered", status=status.HTTP_400_BAD_REQUEST)
+
         try:
             # Connect to the email client
-            email_client = EmailClient.from_connection_string(CONNECTION_STRING)
+            email_client = EmailClient.from_connection_string(CONNECTION_STRINGS[sender])
 
             # Email details
             content = EmailContent(
@@ -22,7 +26,7 @@ class EmailAPI(APIView):
             address = EmailAddress(email=request.data.get('recipient'))
             recipient = EmailRecipients(to=[address])
             message = EmailMessage(
-                sender=request.data.get('sender'),
+                sender=sender,
                 content=content,
                 recipients=recipient
             )
