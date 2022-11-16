33a133a19fc4f0028d19567d3b7612ecf64681d7
tosm01 <taha2013.to@gmail.com>
2022-11-16 14:15:55 +0800

    Added white list for allowed recipients

--- emailservice/api/views.py
+++ emailservice/api/views.py
@@ -10,13 +10,16 @@ from env import REGISTERED_SENDERS_LIST
 class EmailAPI(APIView):
     def post(self, request, *args, **kwargs):
         sender = request.data.get('sender')
+        recipient = request.data.get('recipient')
         
         if sender not in REGISTERED_SENDERS_LIST:
             return HttpResponse(content="Sender is not registered", status=status.HTTP_400_BAD_REQUEST)
+        elif recipient not in REGISTERED_SENDERS_LIST[sender]["allowed_recipients_list"]:
+            return HttpResponse(content="You are not allowed to send an email to the specified recipient", status=status.HTTP_400_BAD_REQUEST)
 
         try:
             # Connect to the email client
-            email_client = EmailClient.from_connection_string(REGISTERED_SENDERS_LIST[sender])
+            email_client = EmailClient.from_connection_string(REGISTERED_SENDERS_LIST[sender]["connection_string"])
 
             # Email details
             content = EmailContent(
