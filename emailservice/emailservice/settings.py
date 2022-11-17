0899d90cced4115480d8999eab56abc1fb3e7a76
tosm01 <taha2013.to@gmail.com>
2022-11-17 11:43:21 +0800

    Removed CORS_ORIGIN_ALLOW_ALL from settings.py

--- emailservice/emailservice/settings.py
+++ emailservice/emailservice/settings.py
@@ -127,5 +127,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ORIGIN_ALLOW_ALL = True
